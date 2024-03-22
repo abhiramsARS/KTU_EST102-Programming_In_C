9c8890c717109bdc5f88038c4efbbe1c49e7b4a7
ABHIRAM_S <149877367+abhiramsARS@users.noreply.github.com>
2024-03-22 20:07:23 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int n,fact=1,i;
+	printf("Enter the Number :-");
+	scanf("%d",&n);
+	
+	for(i=1;i<=n;i++){
+		fact=fact*i;
+	}
+	printf("Factorial of %d = %d",n,fact);
+	printf("\n");
+}
