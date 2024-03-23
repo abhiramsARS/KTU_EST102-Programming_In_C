7c1149cca2c8d0664ccc6dc05bb88c107383813a
ABHIRAM_S <149877367+abhiramsARS@users.noreply.github.com>
2024-03-23 10:48:09 +0530

    Update findAllFactors.c

--- findAllFactors.c
+++ findAllFactors.c
@@ -4,10 +4,10 @@ void main()
   int n,i;
   printf("Enter the Number:-");
   scanf("%d",&n);
-  printf("Factorial of %d :",n)
+  printf("Factorial of %d :",n);
   for(i=1;i<=n;i++){
       if(n%i==0){
-          printf("%d",i);
+          printf("%d\t",i);
       }
   }
 }
