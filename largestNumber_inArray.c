b61df5f72bf1650ca22567597adba2fc890c1889
ABHIRAM_S <149877367+abhiramsARS@users.noreply.github.com>
2024-04-21 15:46:28 +0530

    Update largestNumber_inArray.c

--- largestNumber_inArray.c
+++ largestNumber_inArray.c
@@ -1,1 +1,21 @@
-a
+#include <stdio.h>
+void main() 
+{
+    int l,a[20],i,x,flag=0;
+    printf("Enter length of array -");
+    scanf("%d",&l);
+    for(i=0;i<l;i++){
+        printf("Enter Element :");
+            scanf("%d",&a[i]);
+    }
+    
+    x=a[0];
+    
+    for(i=1;i<l;i++){
+        if (a[i]>x){
+            x=a[i];
+            }
+    }
+    
+    printf("Largest Number in Array =%d",x);
+}
