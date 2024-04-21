9277341a92c204d43ac80fc5eb8ba49816916b59
ABHIRAM_S <149877367+abhiramsARS@users.noreply.github.com>
2024-04-21 15:41:56 +0530

    Update largestNumber_inArray.c

--- largestNumber_inArray.c
+++ largestNumber_inArray.c
@@ -1,1 +1,28 @@
-#include<stdio.h>
+// Online C compiler to run C program online
+#include <stdio.h>
+void main() 
+{
+    int l,a[20],i,x,flag=0;
+    printf("Enter length of array -");
+    scanf("%d",&l);
+    for(i=0;i<l;i++){
+        printf("Enter Element :");
+            scanf("%d",&a[i]);
+    }
+    
+    printf("Enter Element to Search :");
+    scanf("%d",&x);
+    
+    for(i=0;i<l;i++){
+        if (a[i]==x){
+            flag=1;
+            break;
+            }
+    }
+    
+    if (flag){
+        printf("Element Found at %d",i+1);
+    }else{
+        printf("Element Not Found");
+    }
+}
