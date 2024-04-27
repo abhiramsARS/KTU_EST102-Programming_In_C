010415f824f17e68cf660731d17fde3c20748d5c
ABHIRAM_S <149877367+abhiramsARS@users.noreply.github.com>
2024-04-27 15:01:21 +0530

    Update armstrongOrNot.c

--- armstrongOrNot.c
+++ armstrongOrNot.c
@@ -1,1 +1,28 @@
-#include<stdio.h>
+#include <stdio.h>
+void main() 
+{
+    int n,org,c,t=1,sum,i;
+    printf("Enter a number:-");
+    scanf("%d",&n);
+    org=n;
+    while(n>0){           //Counting Number of Digitsin number
+        c++;
+        n=n/10;
+    }
+    n=org;
+    
+    while(n>0){
+        int p=1;
+        t=n%10;
+        for(i=0;i<c;i++){  // Finding t to the power of c (t**c or t^c)
+            p=p*t;
+        }
+        sum=sum+p;
+        n=n/10;
+    }
+    if(sum==org){            //Checking for amstrong
+        printf("Amstrong Number");
+    }else{
+        printf("Not an Amstrong Number");
+    }
+}
