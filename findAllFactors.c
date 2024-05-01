96a1b6923cb908e04683d6a7911f873825d67295
ABHIRAM_S <149877367+abhiramsARS@users.noreply.github.com>
2024-05-01 20:51:02 +0530

    Update findAllFactors.c

--- findAllFactors.c
+++ findAllFactors.c
@@ -1,12 +1,14 @@
+/* Factors of a number
+       - All numbers in range from 0 to n are taken using a for loop, if any of them is divisible by n, they are printed */
 #include<stdio.h>
 void main()
 {
   int n,i;
   printf("Enter the Number:-");
   scanf("%d",&n);
-  printf("Factorial of %d :",n);
+  printf("Factors of %d :",n);
   for(i=1;i<=n;i++){
-      if(n%i==0){
+      if(n%i==0){    
           printf("%d\t",i);
       }
   }
